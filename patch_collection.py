afec563c98d976f4d02be50ebf218be7e4672c1c
Li Jinjing <jinjingx.li@intel.com>
2016-07-27 04:46:00 +0800

    Resove topic patches as other all_deps patches during pulling down

--- patch_collection.py
+++ patch_collection.py
@@ -7,6 +7,7 @@ import argparse
 import subprocess
 
 class Patch(object):
+    """Wrap patch object"""
 
     def __init__(self, Id, number, current_patch, owner, topic=None):
         self.topic = topic
@@ -25,8 +26,10 @@ class Patch(object):
                 self.approvals = None
 
 def get_patch_info(user_id, project):
+    """Get info of qualified(approver=1) patches from gerrit"""
+
     patches = []
-    query_cmd = 'ssh {user}@icggerrit.ir.intel.com -p 29418 gerrit query status:open project:{project} branch:master --current-patch-set --format=JSON approver=1'.format(user=user_id, project=project)
+    query_cmd = 'ssh {user}@icggerrit.corp.intel.com -p 29418 gerrit query status:open project:{project} branch:master --current-patch-set --format=JSON approver=1'.format(user=user_id, project=project)
 
     popen = subprocess.Popen(query_cmd, stdout=subprocess.PIPE, shell=True)
     for i in popen.stdout.readlines():
@@ -46,10 +49,12 @@ def get_patch_info(user_id, project):
     return patches
 
 def check_value(approver_patches):
+    """Filter patches by code-review and approver"""
+
     filtered = []
 
     for i in approver_patches:
-        is_approval = False 
+        is_approval = False
         if i.approvals:
             for j in i.approvals:
                 if j['type'] == "Code-Review" and j['value'] == str(-1):
@@ -59,9 +64,9 @@ def check_value(approver_patches):
                     filtered.append(i)
                     break
                 if j['type'] == "Approver" and j['value'] == str(1):
-                    is_approval = True 
+                    is_approval = True
         else:
-            filtered.append(i) 
+            filtered.append(i)
 
         if not is_approval:
             filtered.append(i)
@@ -73,6 +78,8 @@ def check_value(approver_patches):
     return approver_patches
 
 def find_parents(all_deps, patch_l, obj):
+    """Resolve patch dependencies according to patch revision"""
+
     deps_l=[obj]
     f = False
 
@@ -87,6 +94,9 @@ def find_parents(all_deps, patch_l, obj):
         if obj:
             deps_l.append(obj)
 
+    # all_deps = [[], [], []]
+    # all_deps[n]-> deps_l    [[],[,deps_l],[]]
+    # deps_l <- all_deps[n]   [[], [deps_l, []], []]
     if all_deps:
         for i in all_deps:
             if i[-1].revision == deps_l[0].parents[0]:
@@ -105,6 +115,8 @@ def find_parents(all_deps, patch_l, obj):
     return all_deps, deps_l
 
 def small_bubble(f_list):
+    """Sort(small_bubble) patches according to patch number."""
+
     for j in xrange(len(f_list)-1,-1,-1):
         for i in xrange(j):
             if(f_list[i].number > f_list[i+1].number):
@@ -112,6 +124,11 @@ def small_bubble(f_list):
     return f_list
 
 def find_deps_l(all_deps, num):
+    """
+    Find patches series from all_deps
+    @param all_deps: [[], [],...]
+    @return: deps:[], index of deps in all_deps
+    """
     for deps in all_deps:
         for patchobj in deps:
             if patchobj.number == num:
@@ -119,25 +136,43 @@ def find_deps_l(all_deps, num):
     return None, None
 
 def find_patch(first_deps, topic_patches, all_deps):
+    """Find patch obj from first_deps, topic_patches and all_deps"""
+
     current_head = get_current_head()
     patch_num = []
 
     def find(deps):
-        for i in deps:
-            if i.revision == current_head:
-                patch_num.append(i.number)
+        for dep in deps:
+            if patch_num:
                 break
+            if type(dep) == type([]):
+                for i in dep:
+                    if i.revision == current_head:
+                        patch_num.append(i.number)
+                        break
+            else:
+                if dep.revision == current_head:
+                    patch_num.append(dep.number)
+                    break
 
-    find(first_deps)
-    find(topic_patches)
-    find(all_deps)
+    if first_deps:
+        find(first_deps)
+    if topic_patches:
+        find(topic_patches)
+    if all_deps:
+        find(all_deps)
 
     if patch_num:
-        return patch_num(0)
+        return patch_num[0]
     else:
         return None
 
 def exclude_patches(all_deps, exclude_nums):
+    """
+    Exclude patch from all_deps according to patch num.
+    all_deps: [[a,b],[c,d,e],[f,j]]
+    Result: [[c,d,e],[f,j]] or [[b],[c,d,e],[f,j]]
+    """
     for num in exclude_nums:
         deps_l, deps_l_index = find_deps_l(all_deps, num)
         if deps_l:
@@ -154,6 +189,8 @@ def exclude_patches(all_deps, exclude_nums):
     return all_deps
 
 def boost_priority(all_deps, num1, num2):
+    """Boost patch priority in all_deps,
+    then cherry pick(out) order will be changed"""
 
     deps_1l, deps_1l_index = find_deps_l(all_deps, num1)
     deps_2l, deps_2l_index = find_deps_l(all_deps, num2)
@@ -171,33 +208,73 @@ def boost_priority(all_deps, num1, num2):
 
     return all_deps
 
-def check_out(user_id, project, topic_patches, first_deps):
+def cherry_and_collect(args, deps, successful_s, conflict_s):
+    """
+    Cherry pick patch ony by one from deps, sort them out according to
+    result of cherry pick.
+    @Result: two collect lists
+    """
+
+    for i in deps:
+        print 'Cherry pick patch: ' + i.ref
+        if not cherry_pick(args.name, args.project, i.ref):
+            conflict_s.add(i)
+            ret = os.system('git reset --hard')
+            if ret:
+                print "git reset fails"
+            break
+        else:
+            successful_s.add(i)
+    return successful_s, conflict_s
+
+def pull_patches(args, topic_patches, first_deps, current_head, successful_s, conflict_s):
+    """
+    Cherry out(pick) patches from gerrit to local.
+    1) topic_patches: based on master---[[check out], [cherry pick]]
+                      not based on master --- [[cherry pick],[cherry pick]...]
+                      collect result
+    2) first_deps: [check out, cherry pick....]
+    """
 
     if topic_patches:
-        print "Start to check out the topic patches %s." % topic_patches[0][-1].number
-        check_out_cmd = 'git fetch ssh://%s@icggerrit.ir.intel.com:29418/%s %s && git checkout FETCH_HEAD' % (user_id, project, topic_patches[0][-1].ref)
-        ret = os.system(check_out_cmd)
-        if ret:
-            print "check out topic patches fail"
-            sys.exit(1)
+        if topic_patches[0][0].parents[0] == current_head:
+            print "Start to check out the topic patches %s." % topic_patches[0][-1].number
+            check_out_cmd = 'git fetch ssh://%s@icggerrit.corp.intel.com:29418/%s %s && git checkout FETCH_HEAD' % (args.name, args.project, topic_patches[0][-1].ref)
+            ret = os.system(check_out_cmd)
+            if ret:
+                print "check out topic patches fail"
+            else:
+                for patch in topic_patches[0]:
+                    successful_s.add(patch)
+        else:
+            for topics in topic_patches:
+                successful_s, conflict_s = cherry_and_collect(args, topics, successful_s, conflict_s)
+
+        if topic_patches[1:]:
+            for other in topic_patches[1:]:
+                successful_s, conflict_s = cherry_and_collect(args, other, successful_s, conflict_s)
+
+    #Only the useful for script running for the first time
     elif first_deps:
         print "Start to check out the first patch %s of first dependent patch series." % first_deps[0].number
-        check_out_cmd = 'git fetch ssh://%s@icggerrit.ir.intel.com:29418/%s %s && git checkout FETCH_HEAD' % (user_id, project, first_deps[0].ref)
+        check_out_cmd = 'git fetch ssh://%s@icggerrit.corp.intel.com:29418/%s %s && git checkout FETCH_HEAD' % (args.name, args.project, first_deps[0].ref)
         ret = os.system(check_out_cmd)
         if ret:
             print "check out first patch fail"
-            sys.exit(1)
-        first_deps.pop(0)
+        else:
+            successful_s.add(first_deps[0])
 
-        for i in first_deps:
+        if first_deps[1:]:
             print "Start to cherry other patches of first dependent patch series."
-            cherry_pick(user_id, project, i.ref)
+            successful_s, conflict_s = cherry_and_collect(args, first_deps[1:], successful_s, conflict_s)
+
+    return successful_s, conflict_s
 
 def cherry_pick(user_id, project, ref):
     conflict_F = False
     unmerged_F = False
 
-    cherry_pick_cmd = 'git fetch ssh://{user}@icggerrit.ir.intel.com:29418/{project} {ref} && git cherry-pick -s FETCH_HEAD'.format(user=user_id, project=project, ref=ref)
+    cherry_pick_cmd = 'git fetch ssh://{user}@icggerrit.corp.intel.com:29418/{project} {ref} && git cherry-pick -s FETCH_HEAD'.format(user=user_id, project=project, ref=ref)
 
     print "Start to cherry pick the patch: %s\n" % ref.split('/')[-2]
 
@@ -210,6 +287,7 @@ def cherry_pick(user_id, project, ref):
     if output[1].find('unmerged files') != -1:
         unmerged_F = True
 
+    #Currently, exit in two circumstance
     if popen.returncode:
         print output[1]
         if conflict_F:
@@ -225,33 +303,40 @@ def cherry_pick(user_id, project, ref):
         return True
 
 def push(name, project, topic):
+    """Push local patche in series to gerrit with topic name"""
 
     print "Delete master_backup branch"
     ret = os.system('git branch -D master_backup')
     if ret:
         print "Delete master_backup branch fails."
-        sys.exit(1)
 
     print "Create new master_branchup branch"
     ret = os.system("git checkout -b master_backup")
     if ret:
         print "Checkout new master_backup branch fails."
 
-    push_cmd = "git push ssh://%s@icggerrit.ir.intel.com:29418/%s HEAD:refs/for/master/%s" % (name, project, topic)
+    push_cmd = "git push ssh://%s@icggerrit.corp.intel.com:29418/%s HEAD:refs/for/master/%s" % (name, project, topic)
     print "push_cmd: %s" % push_cmd
     ret = os.system(push_cmd)
     if ret:
         print "Push patches to master branch fail."
+        return False
 
-def review_conflict_patches(user_id, conflict_s, conflict_buffer):
-    for key, value in conflict_buffer.iteritems():
+    return True
+
+def review_conflict_patches(user_id, conflict_s, conflict_buf):
+    """
+    Review confilct patches with comment and code-review -1,
+    the head conflict with is current series head"""
+
+    for key, value in conflict_buf.iteritems():
         for obj in conflict_s:
             if obj.number == key:
-                review_cmd='ssh %s@icggerrit.ir.intel.com -p 29418 gerrit review %s --code-review -1' % (user_id, obj.revision)
+                review_cmd='ssh %s@icggerrit.corp.intel.com -p 29418 gerrit review %s --code-review -1' % (user_id, obj.revision)
                 if value:
-                    msg_cmd='ssh %s@icggerrit.ir.intel.com -p 29418 gerrit review %s -m "\'Conflict with patch %s.\'"' % (user_id, obj.revision, value)
+                    msg_cmd='ssh %s@icggerrit.corp.intel.com -p 29418 gerrit review %s -m "\'Conflict with patch %s.\'"' % (user_id, obj.revision, value)
                 else:
-                    msg_cmd='ssh %s@icggerrit.ir.intel.com -p 29418 gerrit review %s -m "\'Conflict with master.\'"' % (user_id, obj.revision)
+                    msg_cmd='ssh %s@icggerrit.corp.intel.com -p 29418 gerrit review %s -m "\'Conflict with master.\'"' % (user_id, obj.revision)
 
                 print "Give code review -1 onto gerrit for %s" % obj.number
                 ret = os.system(review_cmd)
@@ -269,23 +354,24 @@ def get_current_head():
     for i in p.readlines():
         return i.split(' ')[0]
 
-def find_first_deps(all_deps, current_head, topic):
+def find_first_and_topic_deps(all_deps, current_head, topic):
+    """
+    Find first and topic patch deps from all_deps
+    all_deps -> first:[] + topic:[] + rest[]"""
+
     first_deps = []
     topic_patches_l = []
 
-    def func(deps):
+    for deps in all_deps:
         for patch in deps:
             if patch.topic == topic:
                 topic_patches_l.append(deps)
-                return True
-
-    for deps in all_deps:
-        if func(deps):
-            break
+                break
 
     #Remove topic patches from all_deps
     if topic_patches_l:
-        all_deps.remove(topic_patches_l[0])
+        for topic in topic_patches_l:
+            all_deps.remove(topic)
 
     else:
         #Find first deps which include the first patch that depend on the current master
@@ -298,7 +384,7 @@ def find_first_deps(all_deps, current_head, topic):
     return first_deps, topic_patches_l, all_deps
 
 def get_current_revision(number):
-    cmd = "ssh -p 29418 icggerrit.ir.intel.com gerrit query --current-patch-set --format=JSON %s" % number
+    cmd = "ssh -p 29418 icggerrit.corp.intel.com gerrit query --current-patch-set --format=JSON %s" % number
     pipe = os.popen(cmd)
     jsonStr = pipe.readlines()
     pipe.close()
@@ -308,6 +394,8 @@ def get_current_revision(number):
     return revision
 
 def autoreview(first, successful_s):
+    """Review patches with code-reivew +1 and approver +1 """
+
     approvers = ['liuxiaoz', 'lyang56']
 
     def review_action(patches_l):
@@ -320,13 +408,13 @@ def autoreview(first, successful_s):
                     else:
                         pass
 
-            # Because after push function, revision on gerrit has been changed, which isnot the one queried by get_patch_info function 
+            # Because after push function, revision on gerrit has been changed, which isnot the one queried by get_patch_info function
             revision = get_current_revision(patch.number)
             print "revision: %s" % revision
-            autoreview_cmd = 'ssh %s@icggerrit.ir.intel.com -p 29418 gerrit review %s --code-review +1 --approver +1' % (approvers[id_index], revision)
+            autoreview_cmd = 'ssh %s@icggerrit.corp.intel.com -p 29418 gerrit review %s --code-review +1 --approver +1' % (approvers[id_index], revision)
 
             print "autoreview_cmd: %s" % autoreview_cmd
-            print "Give code review +1 and approver +1 onto gerrit for patch %s" % patch.number 
+            print "Give code review +1 and approver +1 onto gerrit for patch %s" % patch.number
             ret = os.system(autoreview_cmd)
             if ret:
                 print "Autoreview patch %s fails" % patch.number
@@ -335,6 +423,8 @@ def autoreview(first, successful_s):
     review_action(successful_s)
 
 def parse_args():
+    """Parse command"""
+
     parser = argparse.ArgumentParser(description='Patch collection and sorting')
     parser.add_argument('-n', '--name', required=True, help='Username of gerrit')
     parser.add_argument('-p', '--project', required=True, help='Project name')
@@ -368,7 +458,7 @@ if __name__ == '__main__':
     print "===Sort the patches according to patch number.==="
     num_sorted_patches = small_bubble(valued_patches)
 
-    print "===Resolve dependencies.==="
+    print "===Resolve dependencies of patches.==="
     all_deps = []
 
     while len(num_sorted_patches) >= 1:
@@ -384,13 +474,10 @@ if __name__ == '__main__':
         pri_list = args.priority.split(',')
         all_deps = boost_priority(all_deps, pri_list[0], pri_list[1])
 
-    print "===Find first deps and topic patches.==="
-    first_deps, topic_patches, all_deps = find_first_deps(all_deps, current_head, topic)
+    print "===Find first(depend on master patches) or topic patches.==="
+    first_deps, topic_patches, all_deps = find_first_and_topic_deps(all_deps, current_head, topic)
 
-    if not all_deps and not first_deps:
-        print "No patches need to be rebased..."
-        sys.exit(0)
-
+    #Avoid of being on other branches(should on master branch)
     print "===Delete maser branch==="
     ret = os.system('git branch -D master')
     if ret:
@@ -401,39 +488,32 @@ if __name__ == '__main__':
     if ret:
         print "Checkout to master branch fails."
 
-    print "===Start to checkout and cherrypick.==="
-    check_out(args.name, args.project, topic_patches, first_deps)
+    #Patch the patches(from gerrit) locally
+    successful_s = set()
+    conflict_s = set()
+    print "===Start to checkout and cherry pick topic or first patches.==="
+    successful_s, conflict_s = pull_patches(args, topic_patches, first_deps, current_head, successful_s, conflict_s)
 
-    successful_set = set()
-    conflict_set = set()
-    conflict_buffer = {}
-    for index, value in enumerate(all_deps):
-        for i in value:
-            print 'All patches: ' + i.ref
-            if not cherry_pick(args.name, args.project, i.ref):
-                conflict_set.add(i)
-                patch_num = find_patch(first_deps, topic_patches, all_deps)
-                if patch_num:
-                    conflict_buffer[i.number] = patch_num
-                else:
-                    conflict_buffer[i.number] = None
-
-                ret = os.system('git reset --hard')
-                if ret:
-                    print "git reset fails"
-                break
-            else:
-                successful_set.add(i)
+    for deps in all_deps:
+        successful_s, conflict_s = cherry_and_collect(args, deps, successful_s, conflict_s)
 
     print "===Push local patch series to gerrit.==="
-    push(args.name, args.project, topic)
+    push_result = push(args.name, args.project, topic)
 
-    time.sleep(5)
-    print "===Autoreview for patch series.==="
-    autoreview(first_deps, successful_set)
+    if push_result:
+        time.sleep(5)
+        print "===Autoreview for pushed patch series on gerrit .==="
+        autoreview(first_deps, successful_s)
 
-    if conflict_set:
-        print "===Autoreview for conflict patches.==="
-        review_conflict_patches(args.name, conflict_set, conflict_buffer)
+        if conflict_s:
+            conflict_buf = {}
+            print "===Autoreview for conflict patches on gerrit.==="
+            patch_num = find_patch(first_deps, topic_patches, all_deps)
+            if patch_num:
+                conflict_buf[i.number] = patch_num
+            else:
+                conflict_buf[i.number] = None
+
+            review_conflict_patches(args.name, conflict_s, conflict_buf)
 
     print ('Done')
